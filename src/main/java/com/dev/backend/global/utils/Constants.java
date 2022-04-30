b629b5c5af63f2ab08638ba950d1abf8cc322744
POL6463 <kevinkim0617kim@gmail.com>
2022-04-30 22:53:09 +0900

    Feat : url 글로벌 번수 추가

---
+++ src/main/java/com/dev/backend/global/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.dev.backend.global.utils;
+
+public class Constants {
+    public final static String API_PREFIX = "api/v1";
+}
--- src/main/java/com/dev/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dev.backend.repository;
-
-import com.dev.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
