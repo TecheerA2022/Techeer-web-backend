e937473213b223807a85c5719010cdc9b16c695e
POL6463 <kevinkim0617kim@gmail.com>
2022-05-05 00:52:07 +0900

    Feat: 팀 삭제 기능 추가

--- src/main/java/com/dev/backend/domain/team/controller/TeamController.java
+++ src/main/java/com/dev/backend/domain/team/controller/TeamController.java
@@ -15,22 +15,31 @@ import java.util.List;
 
 import static com.dev.backend.global.utils.Constants.API_PREFIX;
 
-@RequestMapping(API_PREFIX)
+@RequestMapping(API_PREFIX + "/teams")
 @RequiredArgsConstructor
 @RestController
 public class TeamController {
     private final TeamService teamService;
 
-    @GetMapping("/teams")
+    @GetMapping
     public List<TeamResponseDto> getList(Model model){
         model.addAttribute("team", teamService.findAll());
         return teamService.findAll();
     }
 
-    @PostMapping("/teams")
+    @PostMapping
     public ResponseEntity save(@RequestBody TeamSaveDto teamSaveDto) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(TeamResponseIdDto.fromEntity(this.teamService.save(teamSaveDto)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        teamService.delete(id);
+        return ResponseEntity
+                .ok()
+                .body(null);
     }
 }
